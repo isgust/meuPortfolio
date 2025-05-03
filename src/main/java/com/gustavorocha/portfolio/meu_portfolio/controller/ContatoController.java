d59326947caf1009d781284705224d1c22a7a8a0
isgust <rocha.gustavops@gmail.com>
2025-05-03 20:41:36 -0300

    correção de dependencias para api do google

--- src/main/java/com/gustavorocha/portfolio/meu_portfolio/controller/ContatoController.java
+++ src/main/java/com/gustavorocha/portfolio/meu_portfolio/controller/ContatoController.java
@@ -1,4 +1,33 @@
 package com.gustavorocha.portfolio.meu_portfolio.controller;
 
+import com.gustavorocha.portfolio.meu_portfolio.model.Contato;
+import com.gustavorocha.portfolio.meu_portfolio.service.ContatoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
 public class ContatoController {
+
+    private final ContatoService contatoService;
+
+    public ContatoController(ContatoService contatoService) {
+        this.contatoService = contatoService;
+    }
+
+    @GetMapping("/contato")
+    public String contatoForm(Model model) {
+        model.addAttribute("contato", new Contato()); // Cria um objeto Contato vazio para o formulário
+        return "contato";
+    }
+
+    @PostMapping("/contato")
+    public RedirectView enviarContato(@ModelAttribute Contato contato, Model model) {
+        contatoService.enviarContato(contato);
+        model.addAttribute("mensagemSucesso", "Sua mensagem foi enviada com sucesso!");
+        return new RedirectView("/contato");
+    }
 }
--- src/main/java/com/gustavorocha/portfolio/meu_portfolio/service/ContatoService.java
+++ src/main/java/com/gustavorocha/portfolio/meu_portfolio/service/ContatoService.java
@@ -1,4 +1,28 @@
 package com.gustavorocha.portfolio.meu_portfolio.service;
+import com.gustavorocha.portfolio.meu_portfolio.model.Contato;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class ContatoService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${GMAIL_USERNAME}")
+    private String destinatarioEmail;
+
+    public void enviarContato(Contato contato){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(destinatarioEmail);
+        message.setSubject("Nova mensagem de contato do seu portfolio");
+        message.setText(String.format("Nome: " + contato.getNome() + " Email: " + contato.getEmail() + " Mensagem: " + contato.getMensagem()));
+
+        mailSender.send(message);
+    }
+
 }

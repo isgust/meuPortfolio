4cce707518a0b36a192e908b28c39e7d5e05e864
isgust <rocha.gustavops@gmail.com>
2025-05-06 11:39:51 -0300

    novo design para index.html

--- src/main/java/com/gustavorocha/portfolio/meu_portfolio/controller/ContatoController.java
+++ src/main/java/com/gustavorocha/portfolio/meu_portfolio/controller/ContatoController.java
@@ -3,10 +3,8 @@ package com.gustavorocha.portfolio.meu_portfolio.controller;
 import com.gustavorocha.portfolio.meu_portfolio.model.Contato;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class ContatoController {

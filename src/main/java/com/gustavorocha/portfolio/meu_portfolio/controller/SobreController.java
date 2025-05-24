8107060e58508fb8900c94e820039cb5fdd53f31
isgust <rocha.gustavops@gmail.com>
2025-05-24 14:28:33 -0300

    delete model Contato e logica em controller, Implementação das principais paginas web sem responsividade

--- src/main/java/com/gustavorocha/portfolio/meu_portfolio/controller/SobreController.java
+++ src/main/java/com/gustavorocha/portfolio/meu_portfolio/controller/SobreController.java
@@ -1,14 +1,13 @@
 package com.gustavorocha.portfolio.meu_portfolio.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller 
+public class SobreController {
 
-@Controller
-public class ContatoController {
-
-    @GetMapping("/contato")
-    public String contato(){
-        return "contato";
+    @GetMapping("/sobre") 
+    public String sobre() {
+        return "sobre"; // Retorna o nome do arquivo HTML
     }
 }
-
